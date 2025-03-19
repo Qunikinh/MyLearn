687eedd4acf755573b33ac568dce81a232aeb371
terry <2208886570@qq.com>
2025-03-19 17:14:26 +0800

    1

---
+++ code/c/c/tzoj/work/b_3.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+
+typedef int Elemtype;
+typedef struct SqStack
+{
+    Elemtype *base;
+    int top;
+    int size;
+}SqStack;
+
+int main(){
+    char a[51];
+    int x;
+    gets(a);
+    
+}
